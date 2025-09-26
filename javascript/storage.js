f75ef9e4965fb74a190e6464d3bfe28abbe53d75
lawrencerosin <lawrencerosin@yahoo.com>
2025-09-26 12:50:10 -0400

    Started implementing the images menu

--- javascript/storage.js
+++ javascript/storage.js
@@ -1,7 +1,8 @@
 const API_KEY="1f272713-85bb-4845-b7f9-da35855fd665";
 
 async function GetAllImages(){
-  const imagesURL=await fetch("https://api.harvardartmuseums.org/image?apikey="+API_KEY);
+  const PATH="https://api.harvardartmuseums.org/image?apikey="+API_KEY
+  const imagesURL=await fetch(PATH);
   const data=await imagesURL.json();
   const submenu=document.getElementById("submenu");
   
@@ -11,6 +12,10 @@ async function GetAllImages(){
     const image=document.createElement("img");
     image.className="background-image-option";
     image.src=record["baseimageurl"];
+    image.addEventListener("click", function(){
+       document.body.style.backgroundImage=`url(${PATH})`;
+       submenu.innerHTML="";
+    })
     submenu.appendChild(image);
   }
 }
